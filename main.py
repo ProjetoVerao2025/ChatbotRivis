6d25f739056d55c481246f1716a4d976502ee4ba
Yanetti <matheusyanetti@gmail.com>
2025-12-08 15:56:47 -0300

    logica main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # logica central de chamar a api, salvar ela em data (para nao ter que fzr o web scrapping duas
 # vezes por dia), depois linkar ela com a classe, e mandar para o zap.
 # agendamento de mensagem atraves de bibliotecas como a prorpia schedule (chama web-scraper e whatsapp)
-
+    
 import schedule
 import time
 from init import save_weekly_data
