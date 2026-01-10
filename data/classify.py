5c3d9620fb67f6ed4e712bc4e0b93e1b07817657
Yanneti2 <m258602@dac.unicamp.br>
2026-01-09 21:49:25 -0300

    classify del + perso_mnsg mod

--- data/classify.py
+++
@@ -1,3 +0,0 @@
-# arquivo deve tratar agora os arquivos com inputs padronizados e nao complexos e classifica-los com
-# base no padrao em 'msgs.json', criando assim um outro json em forma de dicionario em que as keys
-# sejam os valores dos alimentos nos json anteriores e o value a sua devida classificacao.
